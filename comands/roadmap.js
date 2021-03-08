4a1e16064ca07832eccfe02de484900d7ac7cee0
Bernardo01001 <bernardogualberto8@gmail.com>
2021-03-08 10:47:09 -0300

    funcao roadmap

---
+++ comands/roadmap.js
@@ -0,0 +1,3 @@
+module.exports = async (client, msg) => {
+    msg.reply(`Para voce que quer comecar com desenvolvimento recomendo fazer um curso de logica \n Depois escolha uma stack de desenvolvimento e se aprofunde nela \n Se quiser seguir no desenvolvimento web recomendo fazer os cursos da rocketseat \n https://app.rocketseat.com.br/discover/courses/track/guias-estelares`);
+}
